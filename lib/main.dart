68eac95746dcfe17edf3e90afd8815e25741a5b2
abdelrahman mahmoud <https://github.com/abdelrahman-mahmoud-salah>
2024-03-21 19:06:47 +0200

    no message

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:homehand/core/routing/app_router.dart';
+import 'package:homehand/homehand_app.dart';
+
+void main() {
+  runApp(HomeHand(appRouter: AppRouter()));
+}
